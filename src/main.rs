c3dc648be9dee6330d30e89eda81a018452e9c7a
celluloce <celluloce24@gmail.com>
2018-08-21 15:54:48 +0900

    雛形

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate ggez;
+
+use ggez::*;
+
+struct MainState {
+
+}
+
+impl ggez::event::EventHandler for MainState {
+  fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+      Ok(())
+  }
+  fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+      Ok(())
+  }
+}
+
 fn main() {
-    println!("poi-project");
+    let state = &mut MainState { };
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("generative_art", "awesome_person", c).unwrap();
+    event::run(ctx, state).unwrap();
 }
